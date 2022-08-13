84da362f7b68ba4fe0849be5469ca29c56363c1b
Licar <valmarlic@yahoo.com.br>
2022-08-13 10:22:37 -0300

    Primeiro git com linha de comando

---
+++ crud-spring-aluno/src/test/java/com/edu/crud/aluno/CrudSpringAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.crud.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
